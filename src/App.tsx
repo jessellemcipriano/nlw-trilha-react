ce5ce5d3c94eea268cf90d474edcd5edaab0f6a4
Jéssellem S. Cipriano <69681532+jessellemcipriano@users.noreply.github.com>
2021-06-21 18:47:03 -0300

    Aula01
    Criação do projeto e integração com o firebase

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from './components/Button';
+
+function App() {
+  return (
+    <>
+    <h1>Hello manée</h1>
+    <Button/>
+    <Button/>
+    <Button/>
+    </>
+  );
+}
+
+export default App;
